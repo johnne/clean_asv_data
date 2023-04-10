112bf131ac53cc00dfb72cb483f9707a2ba688ef
John Sundh <john.sundh@scilifelab.se>
2023-04-10 10:30:21 +0200

    Use pandas.DataFrame.add to sum clusters

--- src/clean_asv_data/count_clusters.py
+++ src/clean_asv_data/count_clusters.py
@@ -7,46 +7,41 @@ import tqdm
 from clean_asv_data.__main__ import generate_reader, read_clustfile, read_config
 
 
-def count_clusters(clustdf, countsfile, clust_column, chunksize=None, nrows=None):
+def sum_clusters(clustdf, countsfile, clust_column, chunksize=None, nrows=None):
     """
     Calculates sums of clusters in each sample
 
     :param clustdf: Dataframe with ASVs as index and a column with cluster membership
     :param countsfile: Counts of ASVs in each sample
-    :param cluster_column: column name of cluster designation
+    :param clust_column: column name of cluster designation
     :param chunksize: Number of rows to read at a time from the countsfile
     :param nrows: Number of total rows to read (development)
     :return: Dataframe with summed counts per cluster
     """
     reader = generate_reader(f=countsfile, chunksize=chunksize, nrows=nrows)
-    written = 0
-    with sys.stdout as fhout:
-        for i, df in enumerate(
-            tqdm.tqdm(reader, desc="reading counts", unit=" chunks")
-        ):
-            if i == 0:
-                columns = df.columns
-            df_merged = pd.merge(
-                df, clustdf, left_index=True, right_index=True, how="inner"
-            )
-            _cluster_sum = df_merged.groupby(clust_column).sum(numeric_only=True)
-            _cluster_sum.loc[:, columns].to_csv(fhout, sep="\t")
-            written += _cluster_sum.shape[0]
-    return written
+    cluster_sum = pd.DataFrame()
+    for df in tqdm.tqdm(reader, desc="reading counts", unit=" chunks"):
+        merged = pd.merge(clustdf.loc[:, clust_column], df, left_index=True, right_index=True)
+        _cluster_sum = merged.groupby(clust_column).sum(numeric_only=True)
+        cluster_sum = cluster_sum.add(_cluster_sum, fill_value=0)
+    return cluster_sum
 
 
 def main(args):
     # Read config
     args = read_config(args.configfile, args)
+    sys.stderr.write(f"Reading {args.clustfile}\n")
     clustdf = read_clustfile(args.clustfile)
-    written = count_clusters(
+    cluster_sum = sum_clusters(
         clustdf,
         args.countsfile,
         args.clust_column,
         chunksize=args.chunksize,
         nrows=args.nrows,
     )
-    sys.stderr.write(f"Wrote {written} clusters\n")
+    with sys.stdout as fhout:
+        cluster_sum.to_csv(fhout, sep="\t")
+
 
 
 def main_cli():
