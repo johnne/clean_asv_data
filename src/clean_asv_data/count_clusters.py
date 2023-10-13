8ebe9f85281d76810db4adb28420908d9af38dc8
John Sundh <john.sundh@scilifelab.se>
2023-10-13 10:59:35 +0200

    Add subsetting and blank check via metadata

--- src/clean_asv_data/count_clusters.py
+++ src/clean_asv_data/count_clusters.py
@@ -8,11 +8,11 @@ from clean_asv_data.__main__ import (
     generate_reader,
     read_clustfile,
     read_config,
-    read_blanks,
+    read_metadata,
 )
 
 
-def sum_clusters(clustdf, countsfile, clust_column, blanks=None, chunksize=None, nrows=None):
+def sum_clusters(clustdf, countsfile, clust_column, blanks=None, subset=None, chunksize=None, nrows=None):
     """
     Calculates sums of clusters in each sample
 
@@ -25,9 +25,14 @@ def sum_clusters(clustdf, countsfile, clust_column, blanks=None, chunksize=None,
     """
     if blanks is None:
         blanks = []
+    if subset is None:
+        subset = []
     reader = generate_reader(f=countsfile, chunksize=chunksize, nrows=nrows)
     cluster_sum = pd.DataFrame()
     for df in tqdm.tqdm(reader, desc="reading counts", unit=" chunks"):
+        if len(subset)>0:
+            subset_intersection = list(set(subset).intersection(set(df.columns)))
+            df = df.loc[:, subset_intersection]
         merged = pd.merge(
             clustdf.loc[:, clust_column],
             df.drop(blanks, axis=1, errors='ignore'),
@@ -44,12 +49,26 @@ def main(args):
     args = read_config(args.configfile, args)
     sys.stderr.write(f"Reading {args.clustfile}\n")
     clustdf = read_clustfile(args.clustfile)
-    blanks = read_blanks(f=args.blanksfile)
+    # Read metadata
+    metadata = None
+    subset = None
+    if args.metadata:
+        metadata = read_metadata(args.metadata, index_name=args.metadata_index_name)
+        # Extract blanks from metadata
+        if not args.noblanks:
+            blanks = list(metadata.loc[metadata[args.sample_type_col].isin(args.blank_val)].index)
+            sys.stderr.write("####\n" f"Found {len(blanks)} blanks in metadata\n")
+        else:
+            blanks = []
+        if args.subset_val:
+            subset = metadata.loc[metadata[args.subset_col] == args.subset_val].index
+            sys.stderr.write("####\n" f"Found {len(subset)} samples for {args.subset_col}:{args.subset_val}\n")
     cluster_sum = sum_clusters(
         clustdf,
         args.countsfile,
         args.clust_column,
         blanks,
+        subset,
         chunksize=args.chunksize,
         nrows=args.nrows,
     )
@@ -71,6 +90,40 @@ def main_cli():
         "the cluster it belongs to",
     )
     parser.add_argument(
+        "--metadata", type=str, 
+        help="Tab-separated file with metadata for each sample"
+    )
+    parser.add_argument(
+        "--metadata_index_name", type=str, help="Name of column in metadata file that contains sample ids (default: 'sampleID_NGI'))",
+        default="sampleID_NGI"
+    )
+    parser.add_argument(
+        "--sample_type_col",
+        type=str,
+        default="lab_sample_type",
+        help="Use this column in metadata to identify sample type (default 'lab_sample_type')",
+    ),
+    parser.add_argument(
+        "--blank_val",
+        type=str,
+        nargs="+",
+        default=['buffer_blank', 'extraction_neg', 'pcr_neg'],
+        help="Values in <sample_type_col> that identify blanks (default 'buffer_blank', 'extraction_neg', 'pcr_neg')",
+    ),
+    parser.add_argument(
+        "--noblanks",
+        action="store_true",
+        help="Ignore blanks",
+    ),
+    parser.add_argument(
+        "--subset_col", type=str, default="dataset",
+        help="Column in metadata to use for subsetting the counts on (default: 'dataset')"
+    )
+    parser.add_argument(
+        "--subset_val", type=str,
+        help="Value in subset_col to use for subsetting the counts on"
+    )
+    parser.add_argument(
         "--configfile",
         type=str,
         default="config.yml",
@@ -78,11 +131,8 @@ def main_cli():
     )
     parser.add_argument(
         "--clust_column",
-        type=str,
-        help="Name of cluster column. Defaults to 'cluster'",
-    )
-    parser.add_argument(
-        "--blanksfile", type=str, help="File with samples that are 'blanks'"
+        type=str, default="cluster",
+        help="Name of cluster column (default: 'cluster')"
     )
     parser.add_argument(
         "--chunksize",
